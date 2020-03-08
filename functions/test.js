8aab425e172c8c81fae7f5330f082f0680bc2332
spip01 <Sp@farpoint.us>
2020-03-08 07:37:05 -0500

    Release parts selection code

--- functions/test.js
+++ functions/test.js
@@ -14,37 +14,80 @@ async function main() {
         for (let ref of refs) { // galaxy
             // ref.listCollections().then(async refs => {
             // for (let ref of refs) { // type
+            console.log(ref.id)
 
-            ref = ref.collection("Ship")
-            ref = ref.where("Type", "==", "Explorer")
-            ref = ref.where("Asymmetric", "==", false)
-            let snapshot = await ref.get()
+            let sref = ref.collection("Ship")
+            let snapshot = await sref.get()
             console.log(snapshot.size)
 
             for (let doc of snapshot.docs) {
                 let e = doc.data()
-                let parts = {}
 
-                if (e.parts) {
-                    parts = e.parts
-                    const pairs = [16, 20, 13, 5, 14, 15, 18, 17, 21, 24, 19, 30, 22, 25, 28, 23, 29, 31, 27, 26]
-                    let list = Object.keys(e.parts)
+                if (typeof e.parts === "undefined") {
+                    let parts = {}
 
-                    for (let i of list) {
-                        let left = parseInt(i.slice(1))
-                        if (pairs.includes(left))
-                            parts["h" + (100 + left)] = true
+                    switch (e.Type) {
+                        case "Fighter":
+                        case "Hauler":
+                        case "Shuttle":
+                            parts = e.wings ? e.wings : {}
+                            if (e.bodies)
+                                for (let p of Object.keys(e.bodies))
+                                    parts["h" + (parseInt(p.slice(1)) + 100)] = true
+
+                            if (e.Type === "Fighter" && parts.h17 && !e.Asymmetric)
+                                parts.h117 = true
+                            break
+                        case "Exotic":
+                            parts = e.bodies
+                            break
+                        case "Explorer":
+                            parts = e.bodies
+
+                            if (!e.Asymmetric) {
+                                const pairs = [16, 20, 13, 5, 14, 15, 18, 17, 21, 24, 19, 30, 22, 25, 28, 23, 29, 31, 27, 26]
+                                let list = Object.keys(e.parts)
+
+                                for (let i of list) {
+                                    let left = parseInt(i.slice(1))
+                                    if (pairs.includes(left))
+                                        parts["h" + (100 + left)] = true
+                                }
+                            }
+
+                            break
                     }
-                }
 
-                console.log(e.id, JSON.stringify(parts))
-                doc.ref.set({
-                    parts: parts
-                }, {
-                    merge: true
-                })
+                    console.log(e.id, JSON.stringify(parts))
+                    // doc.ref.set({
+                    //     parts: parts
+                    // }, {
+                    //     merge: true
+                    // })
+                }
+            }
+
+            let fref = ref.collection("Freighter")
+            snapshot = await fref.get()
+
+            for (let doc of snapshot.docs) {
+                let e = doc.data()
+                if (typeof e.parts === "undefined") {
+                    let parts = e.common ? e.common : {}
+                    if (e.capital)
+                        for (let p of Object.keys(e.capital))
+                            parts["h" + (parseInt(p.slice(1)) + 100)] = true
+
+                    console.log(e.id, JSON.stringify(parts))
+                    // doc.ref.set({
+                    //     parts: parts
+                    // }, {
+                    //     merge: true
+                    // })
+                }
             }
         }
+
     })
 }
 
